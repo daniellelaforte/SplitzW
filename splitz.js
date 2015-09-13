29c935f13ed1df24a7e4c8b10d2b35b4dfc26d9b
Danielle Laforte <daniellelaforte@Danielles-MacBook-Pro-2.local>
2015-09-13 09:48:05 -0600

    fixing the classes

--- splitz.js
+++ splitz.js
@@ -2,139 +2,125 @@ angular.module("bear", []);
 
 angular.module("bear").controller("polarbearcontroller", ["$scope", function($scope){
 	$scope.message = "Add another price";
-	//$scope.classname="forthehiddenform";
 	$scope.prices=[{amt:"", color:""}]
-	$scope.counter=0;
-	$scope.total=0;
-	$scope.totals=[];
-	$scope.redtotal="";
-	$scope.orangetotal="";
-	$scope.yellowtotal="";
-	$scope.greentotal="";
+	$scope.totals={};
+	$scope.sum=0;
+	
 	
 
 	
 	$scope.Submit = function() {
 			//event.preventDefault(); //don't need this - angular does this automatically
-		   // for (var i=0; i<$scope.prices.length; i++){
-	   	//     $scope.prices[i].amt = $scope.prices[i].amt || 0
-					// $scope.total+=parseFloat($scope.prices[i].amt)
-				
-		   // }
-		  	
-		  	for ($scope.price in $scope.prices){
-		  		    if (!$scope.totals[$scope.price.color])
-		  		    	$scope.totals[$scope.price.color] = 0;
-					$scope.totals[$scope.price.color]+=$scope.price.amt;    
-				
-		   }
-           
-           $scope.totals['red'] = $scope.totals['red'] || 0 ;
-           $scope.totals['orange'] = $scope.totals['orange']  || 0;
-           $scope.totals['yellow'] = $scope.totals['yellow'] || 0;
-           $scope.totals['green'] = $scope.totals['green'] || 0;
+		 
 
-           $scope.redtotal = parseFloat($scope.totals['red']);
-           $scope.orangetotal = $scope.totals['orange'];
-           $scope.yellowtotal = $scope.totals['yellow'];
-           $scope.greentotal = $scope.totals['green'];
+    for (var i = 0; i<$scope.prices.length; i++){
+     	console.log($scope.prices[i].color);
+        console.log($scope.prices[i].amt);
 
-           console.log(parseFloat($scope.totals['red']));
+        if (!$scope.totals[$scope.prices[i].color])
+        	$scope.totals[$scope.prices[i].color] = 0;
+        $scope.totals[$scope.prices[i].color] += parseFloat($scope.prices[i].amt);
+    }
+
+    $scope.tax = parseFloat($scope.tax);
+
+    for (var j = 0; j<$scope.prices.length; j++){
+    	$scope.sum += parseFloat($scope.prices[j].amt);}
+
+    $scope.redtotal = $scope.totals.red + ($scope.totals.red/$scope.sum)*($scope.tax);
+    $scope.orangetotal = $scope.totals.orange;
+    $scope.yellowtotal = $scope.totals.yellow;
+    $scope.greentotal = $scope.totals.green;
+    $scope.bluetotal = $scope.totals.blue;
+    $scope.purpletotal = $scope.totals.purple;
+    $scope.blacktotal = $scope.totals.black;
+    $scope.skybluetotal = $scope.totals.skyblue;
+
 
 		  
 		  
     }
+
+
+
+
  	 $scope.clickLittle = function(index, str) {
-		    	//$scope.classname="forthevisibleform"
-		    	if (str==="red"){
-		    		$scope.littleredboxclicked = "littleredbox";
-		    		$scope.littleorangeboxclicked = "lightorange";
-		    		$scope.littleyellowboxclicked = "lightyellow";
-		   		    $scope.littlegreenboxclicked = "lightgreen";
-		            $scope.littleblueboxclicked = "lightblue";
-		            $scope.littlepurpleboxclicked = "lightpurple";
-		            $scope.littleblackboxclicked = "lightblack";
-		            $scope.littleskyblueboxclicked = "lightskyblue";}
+ 	 	
+		    $scope.prices[index].color=str;
+ 
+      
 
-		        if (str==="orange"){
-		    		$scope.littleredboxclicked = "lightred";
-		    		$scope.littleorangeboxclicked = "littleorangebox";
-		    		$scope.littleyellowboxclicked = "lightyellow";
-		   		    $scope.littlegreenboxclicked = "lightgreen";
-		            $scope.littleblueboxclicked = "lightblue";
-		            $scope.littlepurpleboxclicked = "lightpurple";
-		            $scope.littleblackboxclicked = "lightblack";
-		            $scope.littleskyblueboxclicked = "lightskyblue";}
+		    if (str==="red"){
+		    	 $scope.menured = function(index) {
+		    	 if (index === 0)
+		    	  	return "lightred";
+		    	 }
 
-		            if (str==="yellow"){
-		    		$scope.littleredboxclicked = "lightred";
-		    		$scope.littleorangeboxclicked = "lightorange";
-		    		$scope.littleyellowboxclicked = "littleyellowbox";
-		   		    $scope.littlegreenboxclicked = "lightgreen";
-		            $scope.littleblueboxclicked = "lightblue";
-		            $scope.littlepurpleboxclicked = "lightpurple";
-		            $scope.littleblackboxclicked = "lightblack";
-		            $scope.littleskyblueboxclicked = "lightskyblue";}
+		    }
 
-		            if (str==="green"){
-		    		$scope.littleredboxclicked = "lightred";
-		    		$scope.littleorangeboxclicked = "lightorange";
-		    		$scope.littleyellowboxclicked = "lightyellow";
-		   		    $scope.littlegreenboxclicked = "littlegreenbox";
-		            $scope.littleblueboxclicked = "lightblue";
-		            $scope.littlepurpleboxclicked = "lightpurple";
-		            $scope.littleblackboxclicked = "lightblack";
-		            $scope.littleskyblueboxclicked = "lightskyblue";}
+		    if (str==="orange"){
+		    	 $scope.menuorange = function(index) {
+		    	 if (index === 1)
+		    	  	return "lightorange";
+		    	  }
 
-		            if (str==="blue"){
-		    		$scope.littleredboxclicked = "lightred";
-		    		$scope.littleorangeboxclicked = "lightorange";
-		    		$scope.littleyellowboxclicked = "lightyellow";
-		   		    $scope.littlegreenboxclicked = "lightgreen";
-		            $scope.littleblueboxclicked = "littlebluebox";
-		            $scope.littlepurpleboxclicked = "lightpurple";
-		            $scope.littleblackboxclicked = "lightblack";
-		            $scope.littleskyblueboxclicked = "lightskyblue";}
+		    }
 
-		            if (str==="purple"){
-		    		$scope.littleredboxclicked = "lightred";
-		    		$scope.littleorangeboxclicked = "lightorange";
-		    		$scope.littleyellowboxclicked = "lightyellow";
-		   		    $scope.littlegreenboxclicked = "lightgreen";
-		            $scope.littleblueboxclicked = "lightblue";
-		            $scope.littlepurpleboxclicked = "littlepurplebox";
-		            $scope.littleblackboxclicked = "lightblack";
-		            $scope.littleskyblueboxclicked = "lightskyblue";}
+		     if (str==="yellow"){
+		    	 $scope.menuyellow = function(index) {
+		    	 if (index === 2)
+		    	  	return "lightyellow";
+		    	  }
 
-		            if (str==="black"){
-		    		$scope.littleredboxclicked = "lightred";
-		    		$scope.littleorangeboxclicked = "lightorange";
-		    		$scope.littleyellowboxclicked = "lightyellow";
-		   		    $scope.littlegreenboxclicked = "lightgreen";
-		            $scope.littleblueboxclicked = "lightblue";
-		            $scope.littlepurpleboxclicked = "lightpurple";
-		            $scope.littleblackboxclicked = "littleblackbox";
-		            $scope.littleskyblueboxclicked = "lightskyblue";}
+		    }
 
-		            if (str==="skyblue"){
-		    		$scope.littleredboxclicked = "lightred";
-		    		$scope.littleorangeboxclicked = "lightorange";
-		    		$scope.littleyellowboxclicked = "lightyellow";
-		   		    $scope.littlegreenboxclicked = "lightgreen";
-		            $scope.littleblueboxclicked = "lightblue";
-		            $scope.littlepurpleboxclicked = "lightpurple";
-		            $scope.littleblackboxclicked = "lightblack";
-		            $scope.littleskyblueboxclicked = "littleskybluebox";}
+		     if (str==="green"){
+		    	 $scope.menugreen = function(index) {
+		    	 if (index === 3)
+		    	  	return "lightgreen";
+		    	  }
+
+		    }
+
+		     if (str==="blue"){
+		    	 $scope.menublue = function(index) {
+		    	 if (index === 4)
+		    	  	return "lightblue";
+		    	  }
+
+		    }
+
+		     if (str==="purple"){
+		    	 $scope.menupurple = function(index) {
+		    	 if (index === 5)
+		    	  	return "lightpurple";
+		    	  }
+
+		    }
+
+		     if (str==="black"){
+		    	 $scope.menublack = function(index) {
+		    	 if (index === 6)
+		    	  	return "lightblack";
+		    	  }
+
+		    }
+
+		     if (str==="skyblue"){
+		    	 $scope.menuskyblue = function(index) {
+		    	 if (index === 7)
+		    	  	return "lightskyblue";
+		    	  }
+
+		    }
+		    	
 		           
-		    	$scope.prices[index].color=str;
+		    	
 		    	
 
     }
     $scope.Show = function() {
-		    	//$scope.classname="forthevisibleform"
 		    	
-
-		    	$scope.counter++;  
 		    	$scope.prices.push({amt:"", color:""}); 
 
     }
@@ -142,4 +128,3 @@ angular.module("bear").controller("polarbearcontroller", ["$scope", function($sc
 }]
 );
 
-//I don't know how to bind a color and keep adding fields and then using their inputs 
