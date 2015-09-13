dd4afcfc3aa12ddaada995493b3ee3d49ae0e458
Danielle Laforte <daniellelaforte@Danielles-MacBook-Pro-2.local>
2015-09-13 13:27:01 -0600

    still working with the closure problem

--- splitz.js
+++ splitz.js
@@ -2,12 +2,15 @@ angular.module("bear", []);
 
 angular.module("bear").controller("polarbearcontroller", ["$scope", function($scope){
 	$scope.message = "Add another price";
-	$scope.prices=[{amt:"", color:""}]
+	$scope.prices=[{amt:"Enter Item Price", color:""}]
 	$scope.totals={};
 	$scope.sum=0;
 	
 	
-
+    $scope.removePrice = function(price) {
+    	price.amt = "";
+    	$scope.darker = "morevisible";
+    }
 	
 	$scope.Submit = function() {
 			//event.preventDefault(); //don't need this - angular does this automatically
@@ -27,42 +30,42 @@ angular.module("bear").controller("polarbearcontroller", ["$scope", function($sc
     for (var j = 0; j<$scope.prices.length; j++){
     	$scope.sum += parseFloat($scope.prices[j].amt);}
 
-  	if ($scope.totals['red'] != 0  && $scope.totals['red'] != null)
+  	if ($scope.totals['red'] != 0  && $scope.totals['red'] != null && $scope.totals['red'] != NaN)
    		$scope.redtotal = "Red: " + ($scope.totals.red + ($scope.totals.red/$scope.sum)*($scope.tax));
   	else
   		$scope.redtotal = "";
 
-  	if ($scope.totals['orange'] != 0 && $scope.totals['orange'] != null)
+  	if ($scope.totals['orange'] != 0 && $scope.totals['orange'] != null && $scope.totals['orange'] != NaN)
     	$scope.orangetotal = "Orange: " + ($scope.totals.orange + ($scope.totals.orange/$scope.sum)*($scope.tax));
 	else
   		$scope.orangetotal = "";
 
-	if ($scope.totals['yellow'] != 0 && $scope.totals['yellow'] != null)
+	if ($scope.totals['yellow'] != 0 && $scope.totals['yellow'] != null && $scope.totals['yellow'] != NaN)
     	$scope.yellowtotal = "Yellow: " + ($scope.totals.yellow + ($scope.totals.yellow/$scope.sum)*($scope.tax));
 	else
   		$scope.yellowtotal = "";
 
-	if ($scope.totals['green'] != 0 && $scope.totals['green'] != null)
+	if ($scope.totals['green'] != 0 && $scope.totals['green'] != null && $scope.totals['green'] != NaN)
     	$scope.greentotal = "Green: " + ($scope.totals.green + ($scope.totals.green/$scope.sum)*($scope.tax));
 	else
   		$scope.greentotal = "";
 
-	if ($scope.totals['blue'] != 0 && $scope.totals['blue'] != null)
+	if ($scope.totals['blue'] != 0 && $scope.totals['blue'] != null && $scope.totals['blue'] != NaN)
     	$scope.bluetotal = "Blue: " + ($scope.totals.blue + ($scope.totals.blue/$scope.sum)*($scope.tax));
 	else
   		$scope.bluetotal = "";
 
-	if ($scope.totals['purple'] != 0 && $scope.totals['purple'] != null)
+	if ($scope.totals['purple'] != 0 && $scope.totals['purple'] != null && $scope.totals['purple'] != NaN)
     	$scope.purpletotal = "Purple: " + ($scope.totals.purple + ($scope.totals.purple/$scope.sum)*($scope.tax));
 	else
   		$scope.purpletotal = "";
 
-	if ($scope.totals['black'] != 0 && $scope.totals['black'] != null)
+	if ($scope.totals['black'] != 0 && $scope.totals['black'] != null && $scope.totals['black'] != NaN)
     	$scope.blacktotal = "Black: " + ($scope.totals.black + ($scope.totals.black/$scope.sum)*($scope.tax));
 	else
   		$scope.blacktotal = "";
 
-	if ($scope.totals['skyblue'] != 0 && $scope.totals['skyblue'] != null)
+	if ($scope.totals['skyblue'] != 0 && $scope.totals['skyblue'] != null && $scope.totals['skyblue'] != NaN)
     	$scope.skybluetotal = "Skyblue: " + ($scope.totals.skyblue + ($scope.totals.skyblue/$scope.sum)*($scope.tax));
 	else
   		$scope.skybluetotal = "";
@@ -224,7 +227,7 @@ angular.module("bear").controller("polarbearcontroller", ["$scope", function($sc
     }
     $scope.Show = function() {
 		    	
-		    	$scope.prices.push({amt:"", color:""}); 
+		    	$scope.prices.push({amt:"Enter Item Price", color:""}); 
 
     }
 
