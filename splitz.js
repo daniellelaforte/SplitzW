fff6237caedd624da5049beca366648bb27b1dc0
Student <student@Mac-mini-31.local>
2015-09-28 11:05:08 -0600

    fixed currency filter

--- splitz.js
+++ splitz.js
@@ -22,6 +22,14 @@ angular.module("bear").controller("polarbearcontroller", ["$scope", function($sc
 	$scope.purple[0]=1000;
 	$scope.black[0]=1000;
 	$scope.skyblue[0]=1000;
+	$scope.redtotal1 = false;
+	$scope.orangetotal1 = false;
+	$scope.yellowtotal1 = false;
+	$scope.greentotal1 = false;
+	$scope.bluetotal1 = false;
+	$scope.purpletotal1 = false;
+	$scope.blacktotal1 = false;
+	$scope.skybluetotal1 = false;
 
 	
 	
@@ -44,52 +52,82 @@ angular.module("bear").controller("polarbearcontroller", ["$scope", function($sc
         $scope.totals[$scope.prices[i].color] += parseFloat($scope.prices[i].amt);
     }
    
-    $scope.tax = parseFloat($scope.tax);
+    $scope.tax = parseFloat($scope.tax) || 0;
     
 
     for (var j = 0; j<$scope.prices.length; j++){
     	$scope.sum += parseFloat($scope.prices[j].amt);}
 
-  	if ($scope.totals['red'] != 0  && ($scope.totals['red'] != null && $scope.totals['red'] != NaN))
-   		$scope.redtotal = "Red: " + ($scope.totals.red + ($scope.totals.red/$scope.sum)*($scope.tax));
-  	else
-  		$scope.redtotal = "";
+  	if ($scope.totals['red'] != 0  && ($scope.totals['red'] != null && $scope.totals['red'] != NaN)) {
+   		$scope.redtotal =($scope.totals.red + ($scope.totals.red/$scope.sum)*($scope.tax));
+   		$scope.redtotal1 = true;
+  	}
+  	else{
+  		$scope.redtotal1 = false;
+  		$scope.redtotal = "";}
 
-  	if ($scope.totals['orange'] != 0 && ($scope.totals['orange'] != null && $scope.totals['orange'] != NaN))
-    	$scope.orangetotal = "Orange: " + ($scope.totals.orange + ($scope.totals.orange/$scope.sum)*($scope.tax));
-	else
-  		$scope.orangetotal = "";
+  	if ($scope.totals['orange'] != 0 && ($scope.totals['orange'] != null && $scope.totals['orange'] != NaN)){
+    	$scope.orangetotal = ($scope.totals.orange + ($scope.totals.orange/$scope.sum)*($scope.tax));
+    	$scope.orangetotal1 = true;
+  	}
+ 
+	else{
+  		$scope.orangetotal1 = false;
+  		$scope.orangetotal = "";}
 
-	if ($scope.totals['yellow'] != 0 && ($scope.totals['yellow'] != null && $scope.totals['yellow'] != NaN))
-    	$scope.yellowtotal = "Yellow: " + ($scope.totals.yellow + ($scope.totals.yellow/$scope.sum)*($scope.tax));
-	else
-  		$scope.yellowtotal = "";
+	if ($scope.totals['yellow'] != 0 && ($scope.totals['yellow'] != null && $scope.totals['yellow'] != NaN)){
+    	$scope.yellowtotal =  ($scope.totals.yellow + ($scope.totals.yellow/$scope.sum)*($scope.tax));
+    	$scope.yellowtotal1 = true;
+  	}
+	
+	else{
+  		$scope.yellowtotal1 = false;
+  		$scope.yellowtotal = "";}
 
-	if ($scope.totals['green'] != 0 && ($scope.totals['green'] != null && $scope.totals['green'] != NaN))
-    	$scope.greentotal = "Green: " + ($scope.totals.green + ($scope.totals.green/$scope.sum)*($scope.tax));
-	else
-  		$scope.greentotal = "";
+	if ($scope.totals['green'] != 0 && ($scope.totals['green'] != null && $scope.totals['green'] != NaN)){
+    	$scope.greentotal = ($scope.totals.green + ($scope.totals.green/$scope.sum)*($scope.tax));
+    	$scope.greentotal1 = true;
+  	}
+	
+	else{
+  		$scope.greentotal1 = false;
+  		$scope.greentotal = "";}
 
-	if ($scope.totals['blue'] != 0 && ($scope.totals['blue'] != null && $scope.totals['blue'] != NaN))
-    	$scope.bluetotal = "Blue: " + ($scope.totals.blue + ($scope.totals.blue/$scope.sum)*($scope.tax));
-	else
-  		$scope.bluetotal = "";
+	if ($scope.totals['blue'] != 0 && ($scope.totals['blue'] != null && $scope.totals['blue'] != NaN)){
+    	$scope.bluetotal = ($scope.totals.blue + ($scope.totals.blue/$scope.sum)*($scope.tax));
+    	$scope.bluetotal1 = true;
+  	}
+	
+	else{
+  		$scope.bluetotal1 = false;
+  		$scope.bluetotal = "";}
 
-	if ($scope.totals['purple'] != 0 && ($scope.totals['purple'] != null && $scope.totals['purple'] != NaN))
-    	$scope.purpletotal = "Purple: " + ($scope.totals.purple + ($scope.totals.purple/$scope.sum)*($scope.tax));
-	else
-  		$scope.purpletotal = "";
+	if ($scope.totals['purple'] != 0 && ($scope.totals['purple'] != null && $scope.totals['purple'] != NaN)){
+    	$scope.purpletotal = ($scope.totals.purple + ($scope.totals.purple/$scope.sum)*($scope.tax));
+    	$scope.purpletotal1 = true;
+  	}
+	
+	else{
+  		$scope.purpletotal1 = false;
+  		$scope.purpletotal = "";}
 
-	if ($scope.totals['black'] != 0 && ($scope.totals['black'] != null && $scope.totals['black'] != NaN))
-    	$scope.blacktotal = "Black: " + ($scope.totals.black + ($scope.totals.black/$scope.sum)*($scope.tax));
-	else
-  		$scope.blacktotal = "";
+	if ($scope.totals['black'] != 0 && ($scope.totals['black'] != null && $scope.totals['black'] != NaN)){
+    	$scope.blacktotal = ($scope.totals.black + ($scope.totals.black/$scope.sum)*($scope.tax));
+    	$scope.blacktotal1 = true;
+  	}
+	
+	else{
+  		$scope.blacktotal1 = false;
+  		$scope.blacktotal = "";}
 
-	if ($scope.totals['skyblue'] != 0 && ($scope.totals['skyblue'] != null && $scope.totals['skyblue'] != NaN))
-    	$scope.skybluetotal = "Skyblue: " + ($scope.totals.skyblue + ($scope.totals.skyblue/$scope.sum)*($scope.tax));
-	else
-  		$scope.skybluetotal = "";
-
+	if ($scope.totals['skyblue'] != 0 && ($scope.totals['skyblue'] != null && $scope.totals['skyblue'] != NaN)){
+    	$scope.skybluetotal = ($scope.totals.skyblue + ($scope.totals.skyblue/$scope.sum)*($scope.tax));
+    	$scope.skybluetotal1 = true;
+  	}
+	
+	else{
+  		$scope.skybluetotal1 = false;
+  		$scope.skybluetotal = "";}
 
 		  
 		  
